70498222991be99f0d50f9ec41972bb1236981f9
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-04-29 14:39:55 -0300

    Repetições em JavaScript

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+        console.log(`Contagem ${c}`)
+        c++
+    }while(c <= 6)
+
+// while(c <= 6) {
+//     console.log(`Contagem ${c}`)
+//     c++
+// }
+
+// Melhores soluções
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+// Pior solução
