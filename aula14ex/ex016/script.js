c9012d37a5536c7e311f1f04ef6c88cc1573d1ec
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-04-29 17:27:28 -0300

    Começando a tabuada

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -8,7 +8,9 @@ function contar() {
     var passo = window.document.getElementById('passo')
     var res = window.document.getElementById('saida')
 
-    if (inicio.value <= 0|| fim.value <= 0 || passo.value <= 0 || inicio.value > fim.value) {
+    if (inicio.value <= 0|| fim.value <= 0 || passo.value <= 0 || inicio.value > fim.value) 
+    // Falta de concisão (antes)
+{
         window.alert('Impossivel contar!')
     } else {
 
@@ -16,11 +18,16 @@ function contar() {
         var nufim = Number(fim.value)
         var pass = Number(passo.value)
         var resultado = ''
+        // Não havia colocado uma string vazia, isso fazia o número mudar a cada contagem, e mostrando apenas o último
         while (nuini <= nufim) {
             resultado += `${nuini} &#x1F449; `
+            // Não tinha colocado espaço dentro da string após o emogi
             nuini = nuini + pass
         }
         res.innerHTML = resultado + ('&#9872;')
+        // Não coloquei isso pois não havia colocado o resultado.
     }
 }
+
+// 3 erros no total.
 
