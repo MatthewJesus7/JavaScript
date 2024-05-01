8033deef5b38941d2153517d489b8ee4ffb206f9
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-05-01 12:21:17 -0300

    Tabuada "sozinho"

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,22 @@
+// var criar = window.document.getElementById('mostrarnum')
+
+// criar.addEventListener('click', criartabuada)
+
+// function criartabuada() {
+//     var caixanumero = window.document.getElementById('inumero')
+//     var num = Number(caixanumero.value)
+
+//     if (num == 0) {
+//         window.alert('Insira um valor valido! O valor não pode ser 0.')
+//     } else {
+//         var tabuada = window.document.getElementById('tabuada')
+//         tabuada.innerHTML = ''
+//         for (var i = 1; i <= 10; i++) {
+//             var res = num * i
+//             tabuada.innerHTML += num + " x " + i + " = " + res + "<br>";
+//         }
+//     }
+// }
+
+// // Minha versão com ajuda do gpt
+
