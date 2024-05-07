793a0594e845a58c367ba383a65ed4750a6dd7b3
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-05-07 17:11:08 -0300

    aula 17 introdução aos objetos

---
+++ aula17/objeto01.js
@@ -0,0 +1,8 @@
+let registro = {
+    nome:`Shakira`,
+ sexo:`Feminino`,
+  idade:`16`,
+// também da pra colocar funções aqui dentro.
+}
+console.log(`${registro.nome}, ${registro.sexo}, ${registro.idade}`)
+    
